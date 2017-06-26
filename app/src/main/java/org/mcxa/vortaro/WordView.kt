7aee6bdeb5ab810e183035f8d4b8142d0559d5f3
Ian Mcxa <ianmcxa@tutanota.com>
2017-06-25 20:13:17 -0400

    Filter works. Seach ordering needs to be better

--- app/src/main/java/org/mcxa/vortaro/WordView.kt
+++ app/src/main/java/org/mcxa/vortaro/WordView.kt
@@ -1,7 +1,7 @@
 package org.mcxa.vortaro
 
-import android.content.Context
 import android.support.v7.widget.RecyclerView
+import android.support.v7.util.SortedList
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,8 +10,38 @@ import kotlinx.android.synthetic.main.word_item.view.*
 // model class for a single Word
 data class Word(val es : String, val en : String, val ety : String)
 
-class WordAdapter(val context: Context, val words: List<Word>) :
+class WordAdapter :
         RecyclerView.Adapter<WordAdapter.ViewHolder>() {
+
+    val words = SortedList<Word>(Word::class.java, object: SortedList.Callback<Word>() {
+        override fun areContentsTheSame(oldItem: Word?, newItem: Word?): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areItemsTheSame(item1: Word?, item2: Word?): Boolean {
+            return item1 == item2
+        }
+
+        override fun compare(o1: Word?, o2: Word?): Int {
+            return o1!!.es.compareTo(o2!!.es)
+        }
+
+        override fun onChanged(position: Int, count: Int) {
+            notifyItemRangeChanged(position, count)
+        }
+
+        override fun onInserted(position: Int, count: Int) {
+            notifyItemRangeInserted(position, count)
+        }
+
+        override fun onMoved(fromPosition: Int, toPosition: Int) {
+            notifyItemMoved(fromPosition, toPosition)
+        }
+
+        override fun onRemoved(position: Int, count: Int) {
+            notifyItemRangeRemoved(position, count)
+        }
+    })
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val word = itemView.item_word
@@ -32,6 +62,8 @@ class WordAdapter(val context: Context, val words: List<Word>) :
     }
 
     override fun getItemCount(): Int {
-        return words.size
+        return words.size()
     }
+
+
 }
