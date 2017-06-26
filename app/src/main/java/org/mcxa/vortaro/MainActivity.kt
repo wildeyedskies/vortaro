0fe32629cebfa2a814592e57adac462228fb2457
Zoe <zoenb@mailbox.org>
2017-06-25 20:13:17 -0400

    Filter works. Seach ordering needs to be better

--- app/src/main/java/org/mcxa/vortaro/MainActivity.kt
+++ app/src/main/java/org/mcxa/vortaro/MainActivity.kt
@@ -1,14 +1,19 @@
 package org.mcxa.vortaro
 
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.Scanner
+import java.util.*
 import java.util.zip.GZIPInputStream
+import kotlin.collections.ArrayList
+import kotlin.collections.MutableList
 
 class MainActivity : AppCompatActivity() {
     val DICTIONARY_FILENAME = "vortaro.bin"
+
+    val wordList = Collections.synchronizedList(ArrayList<Word>())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +23,21 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
-        // load the wordlist
-        val words = readWords()
-        val wordAapter = WordAdapter(this, words)
+        //load the words
+        //readWords(baseList, filteredList)
+        val wordAapter = WordAdapter()
+
+        val input = Scanner(GZIPInputStream(this@MainActivity.assets.open(DICTIONARY_FILENAME)))
+
+        //TODO run this is the background
+        while (input.hasNextLine()) {
+            val data = input.nextLine().split(':')
+            //parse the data format
+            // es:en:ety
+            val word = Word(data[0], data[1], data[2])
+            wordList.add(word)
+        }
+        wordAapter.words.addAll(wordList)
 
         word_view.apply{
             adapter = wordAapter
@@ -28,18 +45,25 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun readWords(): List<Word> {
-        val words = ArrayList<Word>()
+    // load the words into the wordlist in the background
+    /*fun readWords() {
+        object : AsyncTask<Void,Void,Void>() {
+            override fun doInBackground(vararg params: Void?): Void {
+                val input = Scanner(GZIPInputStream(this@MainActivity.assets.open(DICTIONARY_FILENAME)))
 
-        val input = Scanner(GZIPInputStream(this.assets.open(DICTIONARY_FILENAME)))
+                while (input.hasNextLine()) {
+                    val data = input.nextLine().split(':')
+                    //parse the data format
+                    // es:en:ety
+                    val word = Word(data[0], data[1], data[2])
+                    wordList.add(word)
+                }
+                return null
+            }
 
-        while (input.hasNextLine()) {
-            val data = input.nextLine().split(':')
-            //parse the data format
-            // es:en:ety
-            words.add(Word(data[0], data[1], data[2]))
-        }
+            override fun onPostExecute(result: Void?) {
 
-        return words
-    }
+            }
+        }.execute()
+    }*/
 }
